50d8b8ba36153f9b980a4bf3ca59100471fae9fd
Sean Giddings <sean@Seans-Mac-mini.local>
2022-03-23 00:22:43 -0400

    removed header

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 	$navbar_scheme   = get_theme_mod( 'navbar_scheme', 'navbar-light bg-light' ); // Get custom meta-value.
 	$navbar_position = get_theme_mod( 'navbar_position', 'static' ); // Get custom meta-value.
 
-	$search_enabled  = get_theme_mod( 'search_enabled', '1' ); // Get custom meta-value.
+	$search_enabled  = get_theme_mod( 'search_enabled', '0' ); // Get custom meta-value.
 ?>
 
 <body <?php body_class(); ?>>
